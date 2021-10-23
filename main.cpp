7f2377403d53c453c6848b3b93f6b4cfdc97f433
vikash2806 <vikashrajendran2806@gmail.com>
2021-10-23 15:43:59 +0530

    user profile program

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
-
-class Students
+class myProfile
 {
-private:
-    string Name;
-    string University;
-    int age;
-
 public:
+    string name;
+    string designation;
+    int age;
+    string course;
 };
 int main()
 {
-    return 0;
+    myProfile p1;
+    cout << "Enter the userName:";
+    cin >> p1.name;
+    cout << "\nEnter the designation:";
+    cin >> p1.designation;
+    cout << "\n Enter the course:";
+    cin >> p1.course;
+    cout << "\n Enter the age:";
+    cin >> p1.age;
+    cout << p1.name << " " << p1.designation << " " << p1.course << " " << p1.age << " ";
 }
