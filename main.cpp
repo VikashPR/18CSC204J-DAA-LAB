151bd47e7d6296e0d722a4583eb604b09c1e4b36
vikash2806 <vikashrajendran2806@gmail.com>
2022-01-25 10:57:10 +0530

    root comparison code added

--- main.cpp
+++ main.cpp
@@ -1,39 +1,72 @@
 #include <iostream>
 using namespace std;
+
 struct Node
 {
     int data;
-    struct Node *next;
+    struct Node *left;
+    struct Node *right;
 };
+
+struct Node *newNode(int data)
+{
+    struct Node *node = (struct Node *)
+        malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+
+    cout << root->data << " ";
+
+    inorder(root->right);
+}
+
+int isIdentical(Node *r1, Node *r2)
+{
+    if (r1 == NULL && r2 == NULL)
+        return 1;
+    else if (r1 != NULL && r2 == NULL)
+        return 0;
+    else if (r1 == NULL && r2 != NULL)
+        return 0;
+    else
+    {
+        if (r1->data == r2->data && isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right))
+            return 1;
+        else
+            return 0;
+    }
+}
+
 int main()
 {
-    Node *head, *ptr;
-    char dec = 'y';
-    head = new Node;
-    ptr = head;
-    while (ptr != NULL)
-    {
-        cout << "Enter the node data: ";
-        cin >> ptr->data;
-        cout << "proceed (y/n): ";
-        cin >> dec;
-        if (dec == 'y')
-        {
-            ptr->next = new Node;
-            ptr = ptr->next;
-        }
-        else
-        {
-            ptr->next = NULL;
-            ptr = NULL;
-        }
-    }
-    cout << "The linked list Data:";
-    ptr = head;
-    while (ptr != NULL)
-    {
-        cout << ptr->data << " ";
-        ptr = ptr->next;
-    }
+    struct Node *r1 = newNode(5);
+    struct Node *r2 = newNode(5);
+
+    r1->left = newNode(2);
+    r1->right = newNode(4);
+    r1->left->left = newNode(1);
+    r1->left->right = newNode(1);
+
+    r2->left = newNode(2);
+    r2->right = newNode(4);
+    r2->left->left = newNode(2);
+    r2->left->right = newNode(1);
+
+    if (isIdentical(r1, r2))
+        cout << "Both BSTs are identical";
+    else
+        cout << "BSTs are not identical";
+
     return 0;
 }
